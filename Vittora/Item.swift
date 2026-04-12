f072f9f33eddd42a0211a6f764f1caf40b75e2c3
Rahul Chandera <rahul.crc@gmail.com>
2026-04-12 14:19:33 +0530

    Initial project setup

---
+++ Vittora/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Vittora
+//
+//  Created by Rahul on 12/04/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
